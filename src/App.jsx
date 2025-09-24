36b9a16b830a34934cb1a676fe923062f42a50c6
William Daniel Dias <daniel.willia@hotmail.com>
2025-09-24 19:42:44 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="min-h-dvh grid place-items-center bg-slate-50">
+      <h1 className="text-3xl font-bold text-slate-800">
+        HELLO TAILWIND + REACT!
+      </h1>
+    </main>
+  );
+}
+
+export default App
