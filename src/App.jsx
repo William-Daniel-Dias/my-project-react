b4acf2ad180699390c8739cc20375e31fef5f5ca
William Daniel Dias <daniel.willia@hotmail.com>
2025-09-24 19:58:35 -0300

    alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 const App = () => {
   return (
-    <main className="min-h-dvh grid place-items-center bg-slate-50">
+    <main className=" bg-slate-50">
       <h1 className="text-3xl font-bold text-slate-800">
         HELLO TAILWIND + REACT!
       </h1>
+
+      <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="rounder-2x1 border p-4 bg-white shadow-sm">Card A</div>
+        <div className="rounder-2x1 border p-4 bg-white shadow-sm">Card A</div>
+        <div className="rounder-2x1 border p-4 bg-white shadow-sm">Card A</div>
+      </div>
     </main>
   );
 }
