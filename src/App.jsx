b4de0df79202a0529eb7adc13b9fd298e8fe32da
William Daniel Dias <daniel.willia@hotmail.com>
2025-09-26 19:19:56 -0300

    arrays

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,44 @@ import { Card } from "./components/Card";
 import { Hello } from "./components/Hello";
 
 const App = () => {
+  const CARDS = [
+    {
+      id: 0,
+      title: 'Card A',
+      description: 'Olá eu sou o filho do card A'
+    },
+    {
+      id: 1,
+      title: 'Card B',
+      description: 'Olá eu sou o filho do card B'
+    },
+    {
+      id: 2,
+      title: 'Card C',
+      description: 'Olá eu sou o filho do card C'
+    }
+  ]
+
   return (
-    <main className=" bg-slate-50">
+    <main className="min-h-dvh grid place-items-center bg-slate-50">
       <h1 className="text-3xl font-bold text-slate-800">
         HELLO TAILWIND + REACT!
       </h1>
 
       <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-
-          <Card title="Card A">
-            <p>Oi, eu sou filho</p>
+        {CARDS.map(item => (
+          <Card key={item.id} title={item.title}>
+            <p>{item.description}</p>
           </Card>
-
-           <Card title="Card B">
-            <p>Oi, eu sou filho</p>
-          </Card>
-
-           <Card title="Card C">
-            <p>Oi, eu sou filho</p>
-          </Card>
-          
+        ))}
       </div>
 
       <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-300 focus:ring-2 focus:ring-blue-500 transition-all">
         Link com foco
       </a>
 
-      <Hello name="William"/>
-    
+      <Hello name="William" />
+
     </main>
   );
 }
