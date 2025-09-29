055fe343436d8e3f0ec621ca06c21180a2aa5e20
William Daniel Dias <daniel.willia@hotmail.com>
2025-09-29 19:58:11 -0300

    alterações route

---
+++ src/pages/Contact/index.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../components/Navbar"
+
+export const ContactPage = () => {
+    return (
+        <main>
+            <Navbar />
+
+            <h1>Contato</h1>
+        </main>
+    )
+}
---
+++ src/pages/Features/index.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components/Navbar"
+
+export const FeaturesPage = () => {
+    return (
+        <main>
+            <Navbar />
+
+            <h1>Features</h1>
+
+        </main>
+    )
+}
